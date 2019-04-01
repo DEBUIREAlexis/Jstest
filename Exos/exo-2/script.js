141d9c830084d466db3a6828eafa2853e3a3ab1c
Alexis <alexisdebuire@gmail.com>
2019-04-01 15:17:24 +0200

    Fin de l'exercice 2

--- Exos/exo-2/script.js
+++ Exos/exo-2/script.js
@@ -1,2 +1,17 @@
 // you can write js here
 console.log('exo-2');
+let isSubscibed=false;//ce cera un Booléen
+let age=prompt('Age?');
+
+if(!isSubscibed){
+    var raceNumber=Math.floor(Math.random()*1000);
+}
+if(age>18 && isSubscibed){
+    console.log('You will race at 9:30 am');
+}else if((age>18 || isSubscibed) && !(age>18 && isSubscibed)){
+    console.log('You will race at 11:00 am '+raceNumber)
+}else if(!(age>18) || !isSubscibed){
+    console.log('You will race at 12:30 '+raceNumber)
+}else{
+    console.log('Go to see the registration desk');
+}
