00b6e41abd2d102ee42fff5809e3161ccf6d245a
Alexis <alexisdebuire@gmail.com>
2019-04-01 14:38:03 +0200

    Exo 1 fini, passage à l'exo 2

--- Exos/exo-1/script.js
+++ Exos/exo-1/script.js
@@ -1,5 +1,6 @@
 // you can write js here
 console.log('hello from file');
-let kelvin=294;
+var kelvin = prompt('Quelle est la température en Kelvin aujourd\'hui ?');
 let celsius=kelvin-273;//la temperature en celsuis est de 273 de moins qu'en kelvin
-let fahrenheit=celsius*(9/5)+32;
+let fahrenheit=celsius*(9/5)+32;//création de la variable et assigniation de sa valeur
+fahrenheit = Math.floor(fahrenheit);
