2531efbd708f74973d3eb6756e58a07c41c62e02
Alexis <alexisdebuire@gmail.com>
2019-04-01 14:28:28 +0200

    1er commit, début de l'exo 1

---
+++ Exos/exo-1/script.js
@@ -0,0 +1,5 @@
+// you can write js here
+console.log('hello from file');
+let kelvin=294;
+let celsius=kelvin-273;//la temperature en celsuis est de 273 de moins qu'en kelvin
+let fahrenheit=celsius*(9/5)+32;
