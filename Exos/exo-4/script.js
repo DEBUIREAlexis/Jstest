c6b03c348f1cb1a0289352a4ec4e95054f9d7a23
Alexis <alexisdebuire@gmail.com>
2019-04-01 17:03:34 +0200

    fin exo 4 et fin de journée

--- Exos/exo-4/script.js
+++ Exos/exo-4/script.js
@@ -3,3 +3,24 @@
 console.log('exo-4');
 
 var secretMessage = ["Learning", "isn't", "about", "what", "you", "get", "easily", "the", "first", "time,", "it's", "about", "what", "you", "can", "figure", "out.", "-2015,", "Chris", "Pine,", "Learn", "JavaScript"];
+secretMessage.pop();
+secretMessage.push('to','program');
+let pos = secretMessage.indexOf('easily');
+secretMessage[pos]='right';
+secretMessage.shift();
+secretMessage.unshift('Programming');
+pos=secretMessage.indexOf('get');
+replaceByKnow(pos);
+pos=secretMessage.indexOf('right');
+replaceByKnow(pos);
+pos=secretMessage.indexOf('the');
+replaceByKnow(pos);
+pos=secretMessage.indexOf('first');
+replaceByKnow(pos);
+pos=secretMessage.indexOf('time');
+replaceByKnow(pos);
+
+console.log(secretMessage.join(' '));
+function replaceByKnow(pos){
+ secretMessage[pos]='know';   
+}
