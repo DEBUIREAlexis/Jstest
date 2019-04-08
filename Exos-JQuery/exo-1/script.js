152fff31bf24be2dedfdfdf66bf474adbc762d6a
Alexis <alexisdebuire@gmail.com>
2019-04-08 09:45:24 +0200

    Fin Exo 1 JQuery

--- Exos-JQuery/exo-1/script.js
+++ Exos-JQuery/exo-1/script.js
@@ -1,1 +1,13 @@
 console.log('exercice 1');
+
+// 1 ) Différence entre le Js et JQuery: jQuery est un framework pour écrire du js plus facilement et rapidement, il condense les utilisations communes du js en methode beacoup plus courte à écrire.
+
+// 2 ) Il faut placer la balise script du Jquery avant la balise "script.js", sinon le script va s'éxecuter sans JQuery entrainant donc une erreur
+
+// 5 ) La ligne "script" du JQuery est indispensable car c'est elle qui déclare que le script contiendra du JQuery, et permet donc son fonctionnement
+
+// 6 ) Il s'agit du $ qui permet de voir le JQuery dans le script
+
+/*$(document).ready(function(){
+	console.log('Je peux maintenant écrire du code JQuery');
+});*/
