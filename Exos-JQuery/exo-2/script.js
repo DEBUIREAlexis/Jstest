18b1c43cbd05b3c6886a8f74a9b6f3c870cac92f
Alexis <alexisdebuire@gmail.com>
2019-04-08 10:16:17 +0200

    Fin exo 2 JQuery

--- Exos-JQuery/exo-2/script.js
+++ Exos-JQuery/exo-2/script.js
@@ -1,2 +1,12 @@
 console.log('exercice 2');
 
+/*	1. Sélectionner le titre du blog : "John's Blog". $("#title")
+	2. Sélectionner tous les éléments du sidemenu de gauche. $(".sidenav")
+	3. Sélectionner le footer. $("footer")
+	4. Sélectionner l'élément qui permet de faire la barre de recherche. $(".form-control") 
+	5. Sélectionner tous les glyphicons de la page. $(".glyphicons")
+	6. Sélectionner tous les labels verts.  $(".label-success")
+	7. Sélectionner tous les paragraphes qui ont comme classe 'intro'. $("p.intro")
+	8. Sélectionner toutes les images avec toutes les icones dont la classe est 'glyphicon-time' $("img.glyphicon-time")
+	9. Sélectionner le premier élément du sidemenu. $(".sidenav").first()
+	10. Bonus : sélectionner un paragraphe sur deux.  */
