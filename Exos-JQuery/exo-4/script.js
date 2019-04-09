b62eb0a706f81a170581e3e096bd55269b3948f4
Alexis <alexisdebuire@gmail.com>
2019-04-09 11:53:47 +0200

    Exo 4 en cours

--- Exos-JQuery/exo-4/script.js
+++ Exos-JQuery/exo-4/script.js
@@ -1,1 +1,42 @@
 console.log("exercice 4");
+
+let panier=0;
+
+$('.btn-danger').on('click',()=>{
+    let $aut=$('.form-control').val();
+alert("Merci "+ $aut +" ,nous vous tiendrons informé des différentes offres");
+});
+
+$('#myNavbar ul li:nth-child(2)').on('dblclick',function(){
+    $(this).hide();
+})
+
+//$article=$("#myNavbar ul li").last().text();
+
+$('.panel-body').on('click',()=>{
+    panier+=1;
+    $('.navbar-nav li a').last().html("<span class='glyphicon glyphicon-shopping-cart'></span>Cart "+panier);
+})
+
+$('.panel-primary img').on('mouseover',()=>{
+    console.clear();
+    console.log("L'utilisateur regarde: Ipads");
+})
+
+$('.panel-danger img').on('mouseover',()=>{
+    console.clear();
+    console.log("L'utilisateur regarde: Computers");
+})
+
+$('.panel-success img').on('mouseover',()=>{
+    console.clear();
+    console.log("L'utilisateur regarde: Mobiles");
+})
+
+
+$('input').on('keyup',function(){
+    if(this.value.length>1){
+        console.clear();
+        console.log(this.value);
+    }
+})
