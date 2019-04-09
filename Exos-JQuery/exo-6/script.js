8861d0da9235b540ccf2b19b4f7f4774bc4d0eb0
Alexis <alexisdebuire@gmail.com>
2019-04-09 17:00:44 +0200

    Début exo 6

--- Exos-JQuery/exo-6/script.js
+++ Exos-JQuery/exo-6/script.js
@@ -1,1 +1,13 @@
 console.log("exercice 6");
+
+$('#connect').on('click',()=>{
+    //alert('sa marche?');
+    console.log($('#mail').val());
+    console.log($('#pwd').val());
+    if($('#mail').val().length<1){
+        alert('cest vide!!');
+    }
+    if($('#pwd').val().length<1){
+        alert('Sa aussi!!');
+    }
+})
