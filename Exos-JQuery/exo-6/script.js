cfa2a29cf8087d31227938ab74b113731d25b67a
Alexis <alexisdebuire@gmail.com>
2019-04-10 11:45:01 +0200

    Exo 6 JQuery fini

--- Exos-JQuery/exo-6/script.js
+++ Exos-JQuery/exo-6/script.js
@@ -2,12 +2,25 @@ console.log("exercice 6");
 
 $('#connect').on('click',()=>{
     //alert('sa marche?');
+    console.clear();
     console.log($('#mail').val());
     console.log($('#pwd').val());
-    if($('#mail').val().length<1){
-        alert('cest vide!!');
+    $('#mailInvalide').remove();
+    $('#pdwInvalide').remove();
+    var regex= new RegExp("[A-Za-z0-9](([_\.\-]?[a-zA-Z0-9]+)*)@([A-Za-z0-9]+)(([_\.\-]?[a-zA-Z0-9]+)*)\.([A-Za-z]{2,})");
+    var mail=$('#mail').val();
+    var pdw=$('#pwd').val();
+    if(regex.test(mail)===false){
+        //alert('cest vide!!');
+        $('.container').after("<div id='mailInvalide' class='alert alert-warning'>Mail Invalide</div>");
     }
-    if($('#pwd').val().length<1){
-        alert('Sa aussi!!');
+
+    if(pdw.length<5){
+        //alert('Sa aussi!!');
+        $('.container').after("<div id='pdwInvalide' class='alert alert-warning'>Password Invalide: 6 caractère minimum</div>");
+    }
+
+    if(mail==='hello@me.com' && pdw==='secret8'){
+        alert("Vous êtes connecté");
     }
 })
